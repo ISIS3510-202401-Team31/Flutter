b390c985d6c6524037ff4c9061bd9b7594a20f37
David Rincon <78225654+drincon13@users.noreply.github.com>
2024-03-13 20:32:45 -0500

    restaurants view connected to DB and refactor of some widgets

--- unifood/lib/view/restaurant/dashboard/views/restaurants.dart
+++ unifood/lib/view/restaurant/dashboard/views/restaurants.dart
@@ -1,74 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:unifood/model/restaurant_entity.dart';
 import 'package:unifood/view/widgets/custom_circled_button.dart';
 import 'package:unifood/view/restaurant/dashboard/widgets/custom_restaurant.dart';
+import 'package:unifood/view_model/restaurant_view_model.dart';
 
 class Restaurants extends StatelessWidget {
-  const Restaurants({super.key});
+  const Restaurants({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
-    final List<Map<String, dynamic>> restaurants = [
-      {
-        'imageUrl': 'assets/images/elcarnal_image.jpg',
-        'logoUrl': 'assets/images/elcarnal_logo.jpeg',
-        'name': 'El Carnal',
-        'isOpen': true,
-        'distance': 2.3,
-        'rating': 3.0,
-        'avgPrice': 25.500,
-      },
-      {
-        'imageUrl': 'assets/images/elcarnal_image.jpg',
-        'logoUrl': 'assets/images/elcarnal_logo.jpeg',
-        'name': 'El Carnal',
-        'isOpen': true,
-        'distance': 2.3,
-        'rating': 3.0,
-        'avgPrice': 25.500,
-      },
-      {
-        'imageUrl': 'assets/images/elcarnal_image.jpg',
-        'logoUrl': 'assets/images/elcarnal_logo.jpeg',
-        'name': 'El Carnal',
-        'isOpen': true,
-        'distance': 2.3,
-        'rating': 3.0,
-        'avgPrice': 25.500,
-      },
-    ];
+    double screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-        appBar: AppBar(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(screenHeight * 0.05),
+        child: AppBar(
           automaticallyImplyLeading: false,
           backgroundColor: Colors.transparent,
           elevation: 0,
           actions: [
             Container(
               margin: const EdgeInsets.only(right: 0),
-              child: Container(
-                padding: const EdgeInsets.only(left: 8),
-                height: 45,
-                width: 138,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: const Color(0xFF965E4E),
-                ),
-                child: const Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Icon(Icons.food_bank, color: Colors.black),
-                    SizedBox(width: 8),
-                    Text(
-                      'UNIFOOD',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontFamily: 'KeaniaOne',
-                          fontSize: 15),
+              child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only( left: screenWidth * 0.015),
+                    height: screenHeight,
+                    width: screenWidth * 0.5,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: const Color(0xFF965E4E),
                     ),
-                  ],
-                ),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Icon(Icons.food_bank, color: Colors.black),
+                        SizedBox(width: 8),
+                        Text(
+                          'UNIFOOD',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'KeaniaOne',
+                            fontSize: 15,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
@@ -88,90 +69,135 @@ class Restaurants extends StatelessWidget {
             ),
           ),
         ),
-        body: Padding(
-          padding: const EdgeInsets.only(top: 10.0, left: 20, right: 20),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(
-                    'Favorites',
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 10.0, left: 20, right: 20),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Favorites',
+                  style: TextStyle(fontFamily: 'KeaniaOne', fontSize: 22),
+                ),
+                IconButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/restaurant_search');
+                  },
+                  icon: const Icon(
+                    Icons.search_rounded,
+                    size: 25,
+                  ),
+                )
+              ],
+            ),
+            Container(
+              padding: const EdgeInsets.only(left: 50, right: 10),
+              height: 2,
+              color: const Color(0xFF965E4E),
+            ),
+            const SizedBox(height: 10),
+            FutureBuilder<List<Restaurant>>(
+              future: RestaurantViewModel().getRestaurants(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: SpinKitThreeBounce(
+                      color: Colors.black,
+                      size: 30.0,
+                    ),
+                  );
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else if (snapshot.hasData) {
+                  final List<Restaurant> favoriteRestaurants = snapshot.data!;
+
+                  return Container(
+                    color: const Color(0xFF965E4E).withOpacity(0.15),
+                    height: screenHeight * 0.338,
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: favoriteRestaurants.map((restaurant) {
+                          return CustomRestaurant(
+                            imageUrl: restaurant.imageUrl,
+                            logoUrl: restaurant.logoUrl,
+                            name: restaurant.name,
+                            isOpen: restaurant.isOpen,
+                            distance: restaurant.distance,
+                            rating: restaurant.rating,
+                            avgPrice: restaurant.avgPrice,
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                  );
+                } else {
+                  return const Center(child: Text('No data available.'));
+                }
+              },
+            ),
+            const SizedBox(height: 20),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(bottom: 7),
+                  child: Text(
+                    'Nearby',
                     style: TextStyle(fontFamily: 'KeaniaOne', fontSize: 22),
                   ),
-                  IconButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/restaurant_search');
-                      },
-                      icon: const Icon(
-                        Icons.search_rounded,
-                        size: 25,
-                      ))
-                ],
-              ),
-              Container(
-                padding: const EdgeInsets.only(left: 50, right: 10),
-                height: 2,
-                color: const Color(0xFF965E4E),
-              ),
-              const SizedBox(height: 10),
-              Container(
-                color: const Color(0xFF965E4E).withOpacity(0.15),
-                height: screenHeight * 0.338,
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: restaurants.map((restaurant) {
-                      return CustomRestaurant(
-                          imageUrl: restaurant['imageUrl'],
-                          logoUrl: restaurant['logoUrl'],
-                          name: restaurant['name'],
-                          isOpen: restaurant['isOpen'],
-                          distance: restaurant['distance'],
-                          rating: restaurant['rating'],
-                          avgPrice: restaurant['avgPrice']);
-                    }).toList(),
-                  ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 7),
-                    child: Text(
-                      'Nearby',
-                      style: TextStyle(fontFamily: 'KeaniaOne', fontSize: 22),
+              ],
+            ),
+            Container(
+              padding: const EdgeInsets.only(left: 50, right: 10),
+              height: 2,
+              color: const Color(0xFF965E4E),
+            ),
+            const SizedBox(height: 10),
+            FutureBuilder<List<Restaurant>>(
+              future: RestaurantViewModel().getRestaurants(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: SpinKitThreeBounce(
+                      color: Colors.black,
+                      size: 30.0,
                     ),
-                  ),
-                ],
-              ),
-              Container(
-                padding: const EdgeInsets.only(left: 50, right: 10),
-                height: 2,
-                color: const Color(0xFF965E4E),
-              ),
-              const SizedBox(height: 10),
-              Container(
-                height: screenHeight * 0.338,
-                color: const Color(0xFF965E4E).withOpacity(0.15),
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: restaurants.map((restaurant) {
-                      return CustomRestaurant(
-                          imageUrl: restaurant['imageUrl'],
-                          logoUrl: restaurant['logoUrl'],
-                          name: restaurant['name'],
-                          isOpen: restaurant['isOpen'],
-                          distance: restaurant['distance'],
-                          rating: restaurant['rating'],
-                          avgPrice: restaurant['avgPrice']);
-                    }).toList(),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ));
+                  );
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else if (snapshot.hasData) {
+                  final List<Restaurant> nearbyRestaurants = snapshot.data!;
+
+                  return Container(
+                    height: screenHeight * 0.338,
+                    color: const Color(0xFF965E4E).withOpacity(0.15),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: nearbyRestaurants.map((restaurant) {
+                          return CustomRestaurant(
+                            imageUrl: restaurant.imageUrl,
+                            logoUrl: restaurant.logoUrl,
+                            name: restaurant.name,
+                            isOpen: restaurant.isOpen,
+                            distance: restaurant.distance,
+                            rating: restaurant.rating,
+                            avgPrice: restaurant.avgPrice,
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                  );
+                } else {
+                  return const Center(child: Text('No data available.'));
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
