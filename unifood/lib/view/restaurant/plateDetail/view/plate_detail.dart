d69746f0cc45cc6a8d0f93e21e6b330bcb052592
marianadiaz179 <m.diaza2@uniandes.edu.co>
2024-04-21 18:22:38 -0500

    Plate detail internet connection state

--- unifood/lib/view/restaurant/plateDetail/view/plate_detail.dart
+++ unifood/lib/view/restaurant/plateDetail/view/plate_detail.dart
@@ -9,13 +9,13 @@ import 'package:unifood/view/restaurant/plateDetail/widgets/ranking_info.dart';
 import 'package:unifood/view/widgets/custom_appbar_builder.dart';
 import 'package:unifood/view_model/plate_controller.dart';
 import 'package:unifood/view_model/review_controller.dart';
+import 'package:connectivity/connectivity.dart';
 
 class PlateDetail extends StatefulWidget {
   final String plateId;
   final String restaurantId;
-  const PlateDetail(
-      {Key? key, required this.plateId, required this.restaurantId})
-      : super(key: key);
+
+  const PlateDetail({Key? key, required this.plateId, required this.restaurantId}) : super(key: key);
 
   @override
   _PlateDetailState createState() => _PlateDetailState();
@@ -23,18 +23,25 @@ class PlateDetail extends StatefulWidget {
 
 class _PlateDetailState extends State<PlateDetail> {
   late Future<List<dynamic>> dataFuture;
+  late bool _isConnected;
 
   @override
   void initState() {
     super.initState();
+    _checkConnectivity();
     dataFuture = fetchData();
   }
 
+  Future<void> _checkConnectivity() async {
+    final connectivityResult = await Connectivity().checkConnectivity();
+    setState(() {
+      _isConnected = connectivityResult != ConnectivityResult.none;
+    });
+  }
+
   Future<List<dynamic>> fetchData() async {
-    final plateInfoData = await PlateController()
-        .getPlateById(widget.plateId, widget.restaurantId);
-    final reviewsData = await ReviewController()
-        .getReviewsByPlateId(widget.plateId, widget.restaurantId);
+    final plateInfoData = await PlateController().getPlateById(widget.plateId, widget.restaurantId);
+    final reviewsData = await ReviewController().getReviewsByPlateId(widget.plateId, widget.restaurantId);
     return [plateInfoData, reviewsData];
   }
 
@@ -69,75 +76,101 @@ class _PlateDetailState extends State<PlateDetail> {
             )
             .build(context),
       ),
-      body: FutureBuilder<List<dynamic>>(
-        future: dataFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: SpinKitThreeBounce(
-                color: Colors.black,
-                size: 30.0,
-              ),
-            );
-          } else if (snapshot.hasError) {
-            return Padding(
-              padding: EdgeInsets.all(screenWidth * 0.03),
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Oops! Something went wrong.\nPlease try again later.',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.red,
-                        fontSize: screenWidth * 0.04,
-                        fontWeight: FontWeight.bold,
+      body: _isConnected
+          ? FutureBuilder<List<dynamic>>(
+              future: dataFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: SpinKitThreeBounce(
+                      color: Colors.black,
+                      size: 30.0,
+                    ),
+                  );
+                } else if (snapshot.hasError) {
+                  return Padding(
+                    padding: EdgeInsets.all(screenWidth * 0.03),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Oops! Something went wrong.\nPlease try again later.',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.red,
+                              fontSize: screenWidth * 0.04,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: screenHeight * 0.02),
+                          IconButton(
+                            icon: Icon(
+                              Icons.refresh,
+                              size: screenWidth * 0.08,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                dataFuture = fetchData();
+                              });
+                            },
+                          ),
+                        ],
                       ),
                     ),
-                    SizedBox(height: screenHeight * 0.02),
-                    IconButton(
-                      icon: Icon(
-                        Icons.refresh,
-                        size: screenWidth * 0.08,
+                  );
+                } else if (snapshot.hasData) {
+                  final data = snapshot.data!;
+                  final Plate plate = data[0];
+                  final List<Review> reviews = data[1];
+                  return NotificationListener<ScrollUpdateNotification>(
+                    onNotification: (notification) {
+                      _onUserInteraction("Plate Detail", "Scroll");
+                      return true;
+                    },
+                    child: Container(
+                      child: SingleChildScrollView(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            PlateInfo(plate: plate),
+                            RankingInfo(characteristics: plate.ranking),
+                            ReviewList(reviews: reviews),
+                          ],
+                        ),
                       ),
-                      onPressed: () {
-                        setState(() {});
-                      },
                     ),
-                  ],
-                ),
-              ),
-            );
-          } else if (snapshot.hasData) {
-            final data = snapshot.data!;
-            final Plate plate = data[0];
-            final List<Review> reviews = data[1];
-            return NotificationListener<ScrollUpdateNotification>(
-              onNotification: (notification) {
-                _onUserInteraction("Plate Detail", "Scroll");
-                return true;
+                  );
+                } else {
+                  return const Center(
+                    child: Text('No data available.'),
+                  );
+                }
               },
-              child: Container(
-                child: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      PlateInfo(plate: plate),
-                      RankingInfo(characteristics: plate.ranking),
-                      ReviewList(reviews: reviews),
-                    ],
+            )
+          : Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'No Internet Connection',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
+                  SizedBox(height: 20.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _checkConnectivity();
+                      });
+                    },
+                    child: Text('Refresh'),
+                  ),
+                ],
               ),
-            );
-          } else {
-            return const Center(
-              child: Text('No data available.'),
-            );
-          }
-        },
-      ),
+            ),
     );
   }
 }
